03e83c40a1c1b42039097b5dfb8ed5755450df0a
Rifat0001 <mahmudrifat764@gmail.com>
2023-06-02 21:35:34 +0600

    doneee

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,7 +6,8 @@ const Header = () => {
         <div>
             <div className="navbar bg-indigo-100 px-36">
                 <div className="flex-1">
-                    <a className="btn btn-ghost normal-case text-2xl text-indigo-700 font-bold">Coffee Master</a>
+                    <Link className='btn btn-ghost normal-case text-2xl text-indigo-700 font-bold' to="/">Coffee Master</Link>
+
                 </div>
                 <div className="flex-none">
                     <Link className='font-bold text-xl mx-8' to="/">Home</Link>
